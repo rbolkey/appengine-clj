0b586566e908e43824297fd5e530bb15122db9da
Richard Bolkey <rbolkey@gmail.com>
2011-05-22 22:56:47 -0500

    Removing cyclic dependency entities->query->entities. Updating a few dependencies.

--- project.clj
+++ project.clj
@@ -6,11 +6,11 @@
             :web-src-dir "http://github.com/r0man/appengine-clj/blob/"
             :web-home "http://r0man.github.com/appengine-clj/"
             :copyright "Copyright (c) 2009, 2010 John D. Hume, Roman Scherer, Jean-Denis Greze, E.Fukamachi"}
-  :dependencies [[com.google.appengine/appengine-api-1.0-sdk "1.4.2"]
-                 [inflections "0.4-SNAPSHOT"]
-                 [org.clojure/clojure "1.2.0"]
+  :dependencies [[com.google.appengine/appengine-api-1.0-sdk "1.4.3"]
+                 [inflections "0.4.3"]
+                 [org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [ring/ring-servlet "0.3.6"]]
+                 [ring/ring-servlet "0.3.8"]]
   :dev-dependencies [[autodoc "0.7.1"  :exclusions [org.clojure/clojure
 						   org.clojure/clojure-contrib
                                                    ant/ant
@@ -21,6 +21,6 @@
                      [com.google.appengine/appengine-local-runtime "1.4.3"]
                      [com.google.appengine/appengine-local-runtime-shared "1.4.3"]
                      [com.google.appengine/appengine-testing "1.4.3"]
-                     [ring/ring-jetty-adapter "0.3.6"]
-                     [swank-clojure "1.2.1"]]
+                     [ring/ring-jetty-adapter "0.3.8"]
+                     [swank-clojure "1.3.1"]]
   :repositories {"maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"})
